a11c73eb9edde2379d7483af9abea951d544e326
Akash <shelkea48@gmail.com>
2024-06-08 23:24:24 +0530

    Learning Python file 003

---
+++ Ex03062024/Day_3.py
@@ -0,0 +1,51 @@
+
+"""user_input = input("Enter your name")
+print(user_input)
+
+First_count= input("Enter_first_count_")
+Second_count = input("Enter_second_count")
+
+count = First_count+Second_count
+print(count)
+
+#Type casting from string to the intiger
+count = int(First_count)+int(Second_count)
+print(count)
+
+#Formating of the string---------------------
+dir = "C:\nomad\file1\file2"
+print(dir)  # this \n taking as next line so avoid this make a raw by using r'.....'
+dir = r'C:\nomad\file1\file2'
+print(dir)
+A= "Akash"
+B= "Kumar"
+print(A,B)
+print(f'Full name is {A} {B}')
+a=input("Enter your input ")
+b=int(a)
+print(type(b))
+print(f'Total of {b} and {B} is {b+4}')
+
+"""
+#List---------------------------------------
+My_list = [1,"Akash",3.14,"Suraj"]
+print(len(My_list))
+print(type(My_list))
+My_list.append("Kumar")
+print(My_list)
+My_list.insert(1,23)
+print(My_list)
+My_list.extend(["Abb","Rp"])
+print(My_list)
+My_list.remove("Kumar")
+print(My_list)
+My_list.pop()
+print(My_list)
+My_list.index("Akash")
+print(My_list)
+My_list.reverse()
+print(My_list)
+#My_list.sort()  it will not support between float and string
+print(My_list)
+
+
