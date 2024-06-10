f5e3853a252a6a17b7d4b09c409309c9462e2fbe
Akash <shelkea48@gmail.com>
2024-06-10 23:38:43 +0530

    Learning Python file day 004 Task

---
+++ Ex03062024/10_06_2024)_day_4_Task.py
@@ -0,0 +1,24 @@
+#Explain the difference between the = operator and the == operator in Python.
+"""= is assign the value into the veriable
+ EX= A = 10
+ == is compare the value of two veriables which is return bool
+ a == 9 """
+
+#What does the ** operator do in Python, and how is it used?
+"""** is used to raising first operand to the power of second operand 
+EX= 4**2 = 16 """
+
+
+#What does the ^ operator do in Python, and in what context is it commonly used?
+"""it is XOR binary operator """
+
+# Task 2-------------------
+#Develop a Python script that calculates the square and cube of a given number. num = 2 sq - 4, c = 8
+
+num = 2
+c =8
+sq = int(num**2)-4
+print(sq)
+cube = c**3
+print(cube)
+
